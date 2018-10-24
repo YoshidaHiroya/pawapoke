358bfb8c6b87d926591d3a38129e3dbf5cbf1bc1
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-24 10:20:41 +0900

    firstcommit

---
+++ Assets/ButonText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButonText : MonoBehaviour {
+	Text Test;
+	GameObject Nishimura;
+	// Use this for initialization
+	void Start()
+	{
+		Nishimura = GameObject.Find ("nishimura");
+
+
+
+
+
+		Test=this.gameObject.GetComponent<Text>();
+		StartCoroutine(SampleCoroutine());
+
+	}
+	IEnumerator SampleCoroutine()
+	{
+		Test.text = "にしむら「野球をしよう」\nカンタ「おじさん、臭いでヤンス」";
+		yield return new WaitUntil(Touch);
+		yield return new WaitWhile(Touch);
+		Test.text = "にしむら「臭くないよ、テント生活してるけどちゃんと洗うものは洗っているからね」";
+		yield return new WaitUntil(Touch);
+		yield return new WaitWhile(Touch);
+		Test.text = "カンタ「なるほどでヤンス」" + "\n" + "カンタ「臭いでヤンス」";
+		yield return new WaitUntil(Touch);
+		yield return new WaitWhile(Touch);
+		Nishimura.GetComponent<nishimura>().ChangeStateToHold();
+		Test.text = "にしむら「だから臭くないって！」";
+		yield return new WaitUntil(Touch);
+		yield return new WaitWhile(Touch);
+		Test.text = "カンタ「臭いでヤンス〜〜〜〜〜！！！！」";
+	}
+	bool Touch()
+	{
+		return Input.anyKeyDown;
+
+	}
+}
---
+++ Assets/nishimura.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class nishimura : MonoBehaviour {
+	public Sprite oneSprite;
+	public Image twoprite;
+	public Image threeSprite;
+	SpriteRenderer MainSpriteRenderer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// このobjectのSpriteRendererを取得
+		MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	// 何かしらのタイミングで呼ばれる
+	public void ChangeStateToHold()
+	{
+		// SpriteRenderのspriteを設定済みの他のspriteに変更
+		// 例) HoldSpriteに変更
+		twoprite = this.GetComponent<Image>();
+		twoprite.sprite = oneSprite;
+		MainSpriteRenderer.sprite = oneSprite;
+
+
+
+	}
+}
