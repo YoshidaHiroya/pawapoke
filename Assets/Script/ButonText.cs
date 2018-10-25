37449171c08c7347d11b27ed76ec8ab2c276b624
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-26 05:03:57 +0900

    secondcommit

--- Assets/Script/ButonText.cs
+++ Assets/Script/ButonText.cs
@@ -7,6 +7,12 @@ public class ButonText : MonoBehaviour {
 	Text Test;
 	GameObject Nishimura;
 	GameObject haikei;
+	GameObject sentaku1;
+	GameObject sentaku2;
+	GameObject sentaku1Text;
+	GameObject sentaku2Text;
+	Text sentaku1text;
+	Text sentaku2text;
 	Image image;
 	public Sprite haikei2;
 	// Use this for initialization
@@ -15,8 +21,13 @@ public class ButonText : MonoBehaviour {
 		Nishimura = GameObject.Find ("nishimura");
 		haikei = GameObject.Find ("haikei");
 		image = haikei.GetComponent<Image> ();
+		sentaku1 = GameObject.Find ("sentaku1");
+		sentaku2 = GameObject.Find ("sentaku2");
+		sentaku1Text = GameObject.Find ("sentaku1Text");
+		sentaku2Text = GameObject.Find ("sentaku2Text");
 
-
+		sentaku1text = sentaku1Text.GetComponent<Text> ();
+		sentaku2text = sentaku2Text.GetComponent<Text> ();
 		Test=this.gameObject.GetComponent<Text>();
 		StartCoroutine(SampleCoroutine());
 
@@ -37,7 +48,13 @@ public class ButonText : MonoBehaviour {
 		yield return new WaitUntil(Touch);
 		yield return new WaitWhile(Touch);
 		Test.text = "カンタ「臭いでヤンス〜〜〜〜〜！！！！」";
-		image.sprite = haikei2;
+		sentaku1.GetComponent<Image>().enabled=true;
+		sentaku2.GetComponent<Image>().enabled=true;
+		sentaku1Text.GetComponent<Text>().enabled=true;
+		sentaku2Text.GetComponent<Text>().enabled=true;
+		sentaku1text.text = "本当のことを言う";
+		sentaku2text.text="ぶん殴る";
+		//image.sprite = haikei2;
 
 	}
 	bool Touch()
---
+++ Assets/bunnaguru.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bunnaguru : MonoBehaviour {
+	Text Test;
+	GameObject Text;
+	GameObject Nishimura;
+	GameObject haikei;
+	GameObject sentaku1;
+	GameObject sentaku2;
+	GameObject sentaku1Text;
+	GameObject sentaku2Text;
+	Text sentaku1text;
+	Text sentaku2text;
+	Image image;
+	public Sprite haikei2;
+	// Use this for initialization
+	void Start () {
+		Nishimura = GameObject.Find ("nishimura");
+		haikei = GameObject.Find ("haikei");
+		image = haikei.GetComponent<Image> ();
+		sentaku1 = GameObject.Find ("sentaku1");
+		sentaku2 = GameObject.Find ("sentaku2");
+		sentaku1Text = GameObject.Find ("sentaku1Text");
+		sentaku2Text = GameObject.Find ("sentaku2Text");
+		Text = GameObject.Find ("Text");
+		sentaku1text = sentaku1Text.GetComponent<Text> ();
+		sentaku2text = sentaku2Text.GetComponent<Text> ();
+		Test=Text.GetComponent<Text>();
+	//	StartCoroutine(SampleCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnClickButton(){
+		Test.text = "あいうえお";
+	}
+	//IEnumerator SampleCoroutine()
+	//{
+		
+	//}
+	bool Touch()
+	{
+		return Input.anyKeyDown;
+
+	}
+}
